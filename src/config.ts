c3afdae2b2ed44e196e4c8dd8d0a93af09a37f76
Fabian *-* <nelsonfabiancs8@gmail.com>
2024-06-03 09:46:00 -0500

    some changes

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ export const PageSize = 14;
 // socialPlatform => userName
 // check components/Header.astro socialConfig for more info
 export const Socials: Record<string, Record<string, string>> = {
-	mail: { url: 'mailto:i@ladit.me' },
-	github: { url: 'https://github.com/ladit/astro-blog-zozo' },
+	mail: { url: 'nelsonfabiancs8@gmail.com' },
+	github: { url: 'https://github.com/Motuas/yipao-frontend' },
 /* 	x: { url: 'https://x.com/userName' },
 	mastodon: { url: 'https://mastodon.social/@userName' },
 	facebook: { url: 'https://facebook.com/userName' },
@@ -22,8 +22,8 @@ export const Socials: Record<string, Record<string, string>> = {
 	youtube: { url: 'https://youtube.com/@userName' },
 	skype: { url: 'https://skype.com/userName' },
 	slack: { url: 'https://slack.com/userName' },
-	messenger: { url: 'https://messenger.com/userName' }, */
-	whatsapp: { url: 'https://whatsapp.com/userName' },
+	messenger: { url: 'https://messenger.com/userName' }, 
+	whatsapp: { url: 'https://whatsapp.com/userName' },*/
 	/* snapchat: { url: 'https://snapchat.com/userName' },
 	line: { url: 'https://line.com/userName' },
 	twitch: { url: 'https://twitch.com/userName' },
@@ -32,13 +32,12 @@ export const Socials: Record<string, Record<string, string>> = {
 	dingding: { url: 'https://dingtalk.com/userName' },
 	zhihu: { url: 'https://zhihu.com/userName' },
 	douban: { url: 'https://douban.com/userName' }, */
-	rss: { url: `/${Base}/rss.xml` },
 };
 
 // doc: https://giscus.app
 // data-theme is auto changed between noborder_light / noborder_gray
 export const GiscusConfig: Record<string, string> = {
-	'data-repo': 'ladit/astro-blog-zozo',
+	'data-repo': 'motuas/yipa-frontend',
 	'data-repo-id': 'R_kgDOLgobXQ',
 	'data-category': 'Announcements',
 	'data-category-id': 'DIC_kwDOLgobXc4Cd_N6',
